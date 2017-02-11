9bde17860639f3ecb6a38a29e4814eb78ec8f132
Tom Spencer <fiznool@gmail.com>
2017-02-11 14:17:08 +0000

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+console.log(ENV.api_server);
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
